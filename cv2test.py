1da5824b9227e196acc6b6d70bc3c2641413b0c2
Harnoor Singh <65222131+Harnoor34@users.noreply.github.com>
2024-05-09 05:49:40 +0530

    Delete cv2test.py

--- cv2test.py
+++
@@ -1,7 +0,0 @@
-from cv2 import VideoCapture, destroyAllWindows
-class Webcam():
-    def __init__(self):
-        self.vc = VideoCapture(0)
-    def end_task(self):
-        self.vc.release()
-        destroyAllWindows()
